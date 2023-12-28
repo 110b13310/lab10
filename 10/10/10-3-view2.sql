4866713bce50f1dd2ca7712997bebccbe89f863e
110b13310 <114041713+110b13310@users.noreply.github.com>
2023-12-28 14:04:02 +0800

    Delete 10 directory

--- 10/10/10-3-view2.sql
+++
@@ -1,9 +0,0 @@
-CREATE VIEW dbo.Em_view2 AS
-SELECT 
-    E.Em_id,
-    E.Em_name,
-    CD.Comp_dep_name
-FROM 
-    Employee E
-JOIN 
-    Comp_dep CD ON E.Em_code = CD.Comp_dep_code;
--- 10/10/10/10-5-1-view.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE Teacher
-DROP COLUMN salary;
